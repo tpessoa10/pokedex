2b423b65b274f53b91147ca955f6554860569ccf
Thiago Pessoa <tpessoa96@gmail.com>
2025-10-24 08:38:02 -0300

    Alterando loading

--- src/app/pokemon/loading.tsx
+++ src/app/pokemon/loading.tsx
@@ -2,7 +2,6 @@ export default function Loading() {
   return (
     <div className="flex items-center w-full justify-center h-screen bg-gray-100">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-      <p className="ml-4 text-xl text-blue-600">Carregando...</p>
     </div>
   )
 }
--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -23,7 +23,6 @@ export function Pagination() {
         <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-50">
           <div className="flex flex-col items-center">
             <div className="w-14 h-14 border-4 border-white border-t-blue-500 rounded-full animate-spin"></div>
-            <p className="text-white mt-3 text-lg font-medium">Carregando...</p>
           </div>
         </div>
       )}
